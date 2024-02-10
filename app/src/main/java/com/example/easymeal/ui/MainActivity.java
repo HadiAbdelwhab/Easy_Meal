b6d8797e5f13eb1109ea6cd10a7e6da7aab88d2f
Hadi_Abdelwhab <105137729+HadiAbdelwhab@users.noreply.github.com>
2024-02-10 21:22:13 +0200

    Completed the second activity

--- app/src/main/java/com/example/easymeal/ui/MainActivity.java
+++ app/src/main/java/com/example/easymeal/ui/MainActivity.java
@@ -1,16 +1,30 @@
 package com.example.easymeal.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
 import com.example.easymeal.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Connect the BottomNavigationView with the NavController
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
 }
--- app/src/main/java/com/example/easymeal/util/Constants.java
+++ app/src/main/java/com/example/easymeal/util/Constants.java
@@ -2,4 +2,5 @@ package com.example.easymeal.util;
 
 public class Constants {
     public static final String BASE_URL="https://www.themealdb.com/api/json/v1/1/";
+    public static final String LOGIN_FRAGMENT="login";
 }
