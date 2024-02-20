18491bdd54e8e008f8ce615c418a6f6f171fb84f
Hadi_Abdelwhab <105137729+HadiAbdelwhab@users.noreply.github.com>
2024-02-20 04:16:17 +0200

    Handled SplashScreenActivity

--- app/src/main/java/com/example/easymeal/auth/SplashScreenActivity.java
+++ app/src/main/java/com/example/easymeal/auth/SplashScreenActivity.java
@@ -8,13 +8,35 @@ import android.os.Handler;
 
 import com.example.easymeal.R;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final long SPLASH_DELAY = 2000; // 2 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                navigateToNextScreen();
+            }
+        }, SPLASH_DELAY);
+    }
 
+    private void navigateToNextScreen() {
+        Intent mainIntent = new Intent(SplashScreenActivity.this, AuthenticationActivity.class);
+        startActivity(mainIntent);
+
+        finish();
     }
 }
